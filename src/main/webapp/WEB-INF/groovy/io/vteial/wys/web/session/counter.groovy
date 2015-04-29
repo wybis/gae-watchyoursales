e252da4c7f810d5d5512073445dfdbdba18509ce
vteial <vteial@gmail.com>
2015-04-29 19:48:27 +0530

    updating latest changes...

--- src/main/webapp/WEB-INF/groovy/io/vteial/wys/web/session/counter.groovy
+++ src/main/webapp/WEB-INF/groovy/io/vteial/wys/web/session/counter.groovy
@@ -1,19 +1,20 @@
 package io.vteial.wys.web.session
 import io.vteial.wys.dto.ResponseDto
 import io.vteial.wys.dto.SessionDto
-import io.vteial.wys.model.Account
 import io.vteial.wys.model.Tran
 import io.vteial.wys.model.TranReceipt
 import io.vteial.wys.model.User
 import io.vteial.wys.model.constants.TransactionType
 import io.vteial.wys.service.SessionService
+import io.vteial.wys.service.exceptions.TransactionException
 
 ResponseDto responseDto = new ResponseDto(type : 0, message : 'success...')
 
 SessionDto sessionUserDto = session[SessionService.SESSION_USER_KEY]
 
+TranReceipt tranReceipt = jsonCategory.parseJson(request, TranReceipt.class)
+
 try {
-	TranReceipt tranReceipt = jsonCategory.parseJson(request, TranReceipt.class)
 
 	double totalAmount = 0, actualTotalAmount = 0, amount = 0
 	tranReceipt.trans.each { tran ->
@@ -58,13 +59,19 @@ try {
 
 	responseDto.data = tranReceipt;
 }
+catch(TransactionException e) {
+	responseDto.type = ResponseDto.ERROR
+	responseDto.message = tranReceipt.errorMessage;
+}
 catch(Throwable t) {
 	responseDto.type = ResponseDto.UNKNOWN
 	responseDto.message = t.message
+
 	StringWriter sw = new StringWriter()
 	PrintWriter pw = new PrintWriter(sw)
 	t.printStackTrace(pw)
 	responseDto.data = sw.toString()
+
 	log.warning(sw.toString())
 }
 
